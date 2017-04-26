a126b4966eb3a523349f09c547d3159388498d67
sandeep <sandi10.96@gmail.com>
2017-04-26 10:39:47 +0530

    java xml files change

--- src/main/java/com/funcart/domain/Customer.java
+++ src/main/java/com/funcart/domain/Customer.java
@@ -1,9 +1,8 @@
-package com.funcart.customer;
+package com.funcart.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
