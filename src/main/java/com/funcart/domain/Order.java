ddcbec17c0335ab335523c6f0426de90182461aa
seema verma <seemaverma1257@gmail.com>
2017-05-19 05:47:56 -0700

    again order

--- src/main/java/com/funcart/domain/Order.java
+++ src/main/java/com/funcart/domain/Order.java
@@ -1,7 +1,4 @@
 package com.funcart.domain;
-
-
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,29 +7,62 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "checkout")
-public class Checkout {
+@Table(name = "Order")
+public class Order {
 	
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
-
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	@Column
 	private String customerName;
 	
 	@Column
-	private int customerPhoneNumber;
+	private int itemId;
+
+	@Column
+	private int customerId;
+	
+	@Column
+	private long customerPhoneNumber;
+	@Column
+	private String billingAddressId;
+	@Column
+	private String shippingAddressId;
+	
+	
+	@Column
+	private Double price;
+	
+	
+	@Column
+	private int quantity;
+
+	
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	
 	public String getCustomerName() {
 		return customerName;
 	}
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
-	public int getcustomerPhoneNumber() {
+	public long getcustomerPhoneNumber() {
 		return customerPhoneNumber;
 	}
-	public void setcustomerPhoneNumber(int customerPhoneNumber) {
+	public void setcustomerPhoneNumber(long customerPhoneNumber) {
 		this.customerPhoneNumber = customerPhoneNumber;
 	}
 	
